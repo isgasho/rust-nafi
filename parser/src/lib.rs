9fc06481b9b7ab23a19e007aec32f22229cd4a17
CAD97 <cad97@cad97.com>
2017-08-17 04:02:37 -0400

    Initial stub

---
+++ parser/src/lib.rs
@@ -0,0 +1,7 @@
+//! Transformation of NAFI source code into AST
+
+#![forbid(bad_style, missing_debug_implementations, unconditional_recursion)]
+#![deny(missing_docs, unsafe_code, unused, future_incompatible)]
+
+#[macro_use]
+extern crate nom;
