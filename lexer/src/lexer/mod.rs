a2282be40abb1ad08a3138e0afcf260e9b225bb9
CAD97 <cad97@cad97.com>
2017-08-18 13:13:17 -0400

    Fix block comments

--- lexer/src/lexer/mod.rs
+++ lexer/src/lexer/mod.rs
@@ -1,5 +1,5 @@
 use tokens::Token;
-use nom::{not_line_ending, line_ending, space};
+use nom::{line_ending, not_line_ending, space, IResult};
 
 mod literals;
 use self::literals::integer_literal;
@@ -60,28 +60,43 @@ named! {
     take_line_comment<&str, ()>,
     do_parse!(
         tag!("//") >>
-        content: not_line_ending >>
+        not_line_ending >>
         ()
     )
 }
 
 /// Consume a block comment
-named! {
-    take_block_comment<&str, ()>,
-    do_parse!(
-        tag!("/*") >>
-        many0!(
-            alt_complete!(
-                take_block_comment |
-                do_parse!(
-                    not!(tag!("*/")) >>
-                    ()
-                )
-            )
-        ) >>
-        alt_complete!(tag!("*/") | eof!()) >>
-        ()
-    )
+fn take_block_comment(i: &str) -> IResult<&str, ()> {
+    match tag!(i, "/*") {
+        IResult::Done(mut i, _) => {
+            let mut depth = 1;
+            while depth > 0 {
+                match take!(i, 2) {
+                    IResult::Done(_i, o) if o == "/*" => {
+                        depth += 1;
+                        i = _i;
+                    },
+                    IResult::Done(_i, o) if o == "*/" => {
+                        depth -= 1;
+                        i = _i;
+                    },
+                    IResult::Done(..) => {
+                        i = take!(i, 1).unwrap().0;
+                    },
+                    IResult::Incomplete(_) => {
+                        assert!(
+                            IResult::<&str, &str, u32>::is_done(&eof!(i,),) ||
+                                IResult::<&str, &str, u32>::is_done(&eof!(take!(i, 1).unwrap().0,),)
+                        );
+                        break;
+                    },
+                    e @ IResult::Error(_) => return e.map(|_| ()),
+                }
+            }
+            IResult::Done(i, ())
+        },
+        result => result.map(|_| ()),
+    }
 }
 
 /// Consume as much whitespace as possible
@@ -102,8 +117,20 @@ named! {
 mod test {
     extern crate nom;
     use lexer;
+
     #[test]
     fn block_comment() {
-        assert_eq!(lexer::take_block_comment("/*/**/*/"), nom::IResult::Done("", ()));
+        assert_eq!(
+            lexer::take_block_comment("/** /* */*/"),
+            nom::IResult::Done("", ())
+        );
+    }
+
+    #[test]
+    fn line_comment() {
+        assert_eq!(
+            lexer::take_line_comment("// any amount of stupid text you want"),
+            nom::IResult::Done("", ())
+        );
     }
 }
