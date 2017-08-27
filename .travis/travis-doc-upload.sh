202299d69e6b0549c9d459cb81d4837dd1e10323
CAD97 <cad97@cad97.com>
2017-08-27 00:50:53 -0400

    [Travis] attempt to fix script launch

--- .travis/travis-doc-upload.sh
+++ .travis/travis-doc-upload.sh
@@ -11,7 +11,7 @@ mkdir -p ~/.ssh
 openssl aes-256-cbc \
   -K ${encrypted_6c6066c61167_key} \
   -iv ${encrypted_6c6066c61167_iv} \
-  -in .travis/id_rsa.enc \
+  -in .travis/travis_id_rsa.enc \
   -out ~/.ssh/id_rsa \
   -d
 chmod 600 ~/.ssh/id_rsa
