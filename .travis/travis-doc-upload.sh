ed1d9aeb6afa9d050457c9d8056d0b1d7eefa533
CAD97 <cad97@cad97.com>
2017-08-27 00:56:46 -0400

    [Travis] reencrypt id_rsa

--- .travis/travis-doc-upload.sh
+++ .travis/travis-doc-upload.sh
@@ -7,15 +7,6 @@ set -ev
 
 echo "<meta http-equiv=refresh content=0; url=nafi_lexer/index.html>" > target/doc/index.html
 
-mkdir -p ~/.ssh
-openssl aes-256-cbc \
-  -K ${encrypted_6c6066c61167_key} \
-  -iv ${encrypted_6c6066c61167_iv} \
-  -in .travis/travis_id_rsa.enc \
-  -out ~/.ssh/id_rsa \
-  -d
-chmod 600 ~/.ssh/id_rsa
-
 git clone --branch gh-pages git@github.com:${TRAVIS_REPO_SLUG} docs
 
 cd docs
