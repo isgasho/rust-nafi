5fec077a49b604c2da47a64aa41d7ca876762fa4
CAD97 <cad97@cad97.com>
2017-08-27 02:46:59 -0400

    Remove old files before moving in the new ones

--- .travis/travis-doc-upload.sh
+++ .travis/travis-doc-upload.sh
@@ -5,12 +5,15 @@ set -ev
 [ ${TRAVIS_BRANCH} = master ]
 [ ${TRAVIS_PULL_REQUEST} = false ]
 
-echo "<meta http-equiv=\"refresh\" content=\"0; url=/rust-nafi/nafi_lexer/\">
-<a href=\"/rust-nafi/nafi_lexer/\">Redirect</a>" > target/doc/index.html
+echo "<meta http-equiv=\"refresh\" content=\"0; url=nafi_lexer/index.html\">
+<a href=\"nafi_lexer/index.html\">Redirect</a>" > target/doc/index.html
 
 git clone --branch gh-pages git@github.com:${TRAVIS_REPO_SLUG} docs
 
 cd docs
+shopt -s extglob
+rm -rf !(.git/)
+shopt -u extglob
 mv ../target/doc/* .
 git add -A
 git commit -m "Automatic Travis doc build"
