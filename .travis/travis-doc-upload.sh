c0b8b37037a1794d0d59666fd86d529adeb72e6f
CAD97 <cad97@cad97.com>
2017-08-27 02:33:15 -0400

    Move docs up to root directory rather than in /doc

--- .travis/travis-doc-upload.sh
+++ .travis/travis-doc-upload.sh
@@ -5,12 +5,13 @@ set -ev
 [ ${TRAVIS_BRANCH} = master ]
 [ ${TRAVIS_PULL_REQUEST} = false ]
 
-echo "<meta http-equiv=refresh content=0; url=nafi_lexer/index.html>" > target/doc/index.html
+echo "<meta http-equiv=\"refresh\" content=\"0; url=/nafi_lexer/\">
+<a href=\"/nafi_lexer/\">Redirect</a>" > target/doc/index.html
 
 git clone --branch gh-pages git@github.com:${TRAVIS_REPO_SLUG} docs
 
 cd docs
-mv ../target/doc .
+mv ../target/doc/* .
 git add -A
 git commit -m "Automatic Travis doc build"
 git push origin gh-pages
