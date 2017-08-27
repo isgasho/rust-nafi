cf1095d21794512947fe67436ed9148e0c449dbe
CAD97 <cad97@cad97.com>
2017-08-27 13:33:47 -0400

    Attempt smarter gh-pages deployment

--- .travis/travis-doc-upload.sh
+++ .travis/travis-doc-upload.sh
@@ -5,14 +5,12 @@ set -ev
 [ ${TRAVIS_BRANCH} = master ]
 [ ${TRAVIS_PULL_REQUEST} = false ]
 
-echo "<meta http-equiv=\"refresh\" content=\"0; url=nafi_lexer/index.html\">
-<a href=\"nafi_lexer/index.html\">Redirect</a>" > target/doc/index.html
-
 git clone --branch gh-pages git@github.com:${TRAVIS_REPO_SLUG} docs
-
 cd docs
-ls | xargs rm -r
+
+find -maxdepth 1 -not -name "index.html" -not -name ".git" | xargs -verbose rm -vr
 mv -f ../target/doc/* .
-git add -A
-git commit -m "Automatic Travis doc build"
+
+git add -vA
+git commit -vm "Automatic Travis doc build"
 git push origin gh-pages
