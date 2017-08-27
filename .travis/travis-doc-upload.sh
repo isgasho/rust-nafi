bc51c2550cb796ea00521304c95724764d7b726e
CAD97 <cad97@cad97.com>
2017-08-27 02:36:14 -0400

    Fix redirect to wrong page

--- .travis/travis-doc-upload.sh
+++ .travis/travis-doc-upload.sh
@@ -5,8 +5,8 @@ set -ev
 [ ${TRAVIS_BRANCH} = master ]
 [ ${TRAVIS_PULL_REQUEST} = false ]
 
-echo "<meta http-equiv=\"refresh\" content=\"0; url=/nafi_lexer/\">
-<a href=\"/nafi_lexer/\">Redirect</a>" > target/doc/index.html
+echo "<meta http-equiv=\"refresh\" content=\"0; url=nafi_lexer/\">
+<a href=\"nafi_lexer/\">Redirect</a>" > target/doc/index.html
 
 git clone --branch gh-pages git@github.com:${TRAVIS_REPO_SLUG} docs
 
