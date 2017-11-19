700efe285eaf0d6431d0d535322dcd866fccd55e
Christopher Durham <cad97@cad97.com>
2017-11-19 02:00:07 -0500

    [travis] build+deploy docs

---
+++ .travis/doc_upload.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+set -ev
+
+[ ${TRAVIS_BRANCH} = master ]
+[ ${TRAVIS_PULL_REQUREST} = false ]
+
+git clone --branch gh-pages git@github.com:${TRAVIS_REPO_SLUG} docs
+cd docs
+
+ls | grep --invert-match "index.html" | xargs --verbose rm -r
+mv -f ../target/doc/* .
+
+git add -vA
+git commit -vm "Automatic Travis doc build"
+git push origin gh-pages
