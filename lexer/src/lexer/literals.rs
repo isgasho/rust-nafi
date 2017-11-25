37c53b30352a270540614187774a8ef5315f31fc
Christopher Durham <cad97@cad97.com>
2017-11-25 02:09:33 -0500

    Turns out asking for tokens is the simpler approach

--- lexer/src/lexer/literals.rs
+++ lexer/src/lexer/literals.rs
@@ -1,5 +1,5 @@
 use super::unicode::decimal_number;
-use lexer::tokens;
+use lexer::token;
 use nnom::{ParseOutput, ParseResult};
 use nnom::slice::PositionedStr;
 use std::{char, u32};
@@ -20,7 +20,6 @@ pub fn integer_literal(input: PositionedStr) -> ParseResult<PositionedStr, Token
     )
 }
 
-// FIXME BUG: String interpolation containing a string with unbalanced {} breaks everything help
 // TODO: Break single-character escapes into reusable fn and build character_literal
 /// `Token::Literal(Literal::String)`
 pub fn string_literal(input: PositionedStr) -> ParseResult<PositionedStr, Token, ()> {
@@ -75,18 +74,28 @@ pub fn string_literal(input: PositionedStr) -> ParseResult<PositionedStr, Token,
                         remaining_input = remaining_input.split_at(1).1;
                     },
                     Some('{') => {
-                        let block = grab_matched_substring('{', '}')(remaining_input)
-                            .unwrap()
-                            .output;
-                        let Ok(ParseOutput { mut output, .. }) = tokens(block);
-                        // Remove leading bracket
-                        output.remove(0);
-                        // Remove trailing bracket (can be absent if closed by EOF)
-                        if let Some(&Token::Symbol(_, Symbol::RightCurlyBracket)) = output.last() {
-                            output.pop();
+                        remaining_input = remaining_input.split_at(1).1;
+                        let mut tokens: Vec<Token> = vec![];
+                        let mut depth = 1u32;
+                        while let Ok(ParseOutput {
+                            remaining_input: tail,
+                            output,
+                        }) = token(remaining_input)
+                        {
+                            remaining_input = tail;
+                            match output {
+                                Token::Symbol(_, Symbol::LeftCurlyBracket) => depth += 1,
+                                Token::Symbol(_, Symbol::RightCurlyBracket) => {
+                                    depth -= 1;
+                                    if depth == 0 {
+                                        break;
+                                    }
+                                },
+                                _ => {},
+                            }
+                            tokens.push(output);
                         }
-                        string.push_interpolation(output);
-                        remaining_input = remaining_input.split_at(block.len()).1;
+                        string.push_interpolation(tokens);
                     },
                     Some(s) => string.push_invalid_escape(s.to_string()),
                     None => break,
@@ -108,46 +117,4 @@ pub fn string_literal(input: PositionedStr) -> ParseResult<PositionedStr, Token,
         remaining_input,
         output: Token::Literal(input.start(), string.into()),
     })
-}
-
-fn grab_matched_substring(
-    open: char,
-    close: char,
-) -> impl Fn(PositionedStr) -> ParseResult<PositionedStr, PositionedStr, ()> {
-    move |input: PositionedStr| {
-        if !input.starts_with(open) {
-            return Err(());
-        }
-
-        let mut remaining_input = input.split_at(open.len_utf8()).1;
-        let mut depth = 1u32;
-
-        loop {
-            match remaining_input.chars().next() {
-                Some(ch) if ch == close => {
-                    remaining_input = remaining_input.split_at(ch.len_utf8()).1;
-                    depth -= 1;
-                    if depth == 0 {
-                        break;
-                    }
-                },
-                Some(ch) if ch == open => {
-                    remaining_input = remaining_input.split_at(ch.len_utf8()).1;
-                    depth += 1;
-                },
-                Some(_) => {
-                    let idx = remaining_input
-                        .find(|ch| ch == open || ch == close)
-                        .unwrap_or_else(|| remaining_input.len());
-                    remaining_input = remaining_input.split_at(idx).1;
-                },
-                None => break,
-            }
-        }
-
-        Ok(ParseOutput {
-            remaining_input,
-            output: input.split_at(remaining_input.start() - input.start()).0,
-        })
-    }
 }
