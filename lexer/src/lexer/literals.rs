3f2643db7108082d62699cfefcb720e46f8a7814
Christopher Durham <cad97@cad97.com>
2017-12-16 23:26:40 -0500

    Persist position information within string interpolation

--- lexer/src/lexer/literals.rs
+++ lexer/src/lexer/literals.rs
@@ -16,79 +16,74 @@ pub fn integer_literal(i: Span) -> IResult<Span, Token> {
 }
 
 /// `Token::Literal(Literal::String)`
-pub fn string_literal(i: Span) -> IResult<Span, Token> {
-    // TODO: Do the whole transformation with Span rather than &str
-    let mut slice = i.fragment;
+pub fn string_literal(mut i: Span) -> IResult<Span, Token> {
+    let pos = i.offset;
 
-    if !slice.starts_with('"') {
+    if !i.fragment.starts_with('"') {
+        #[cfg_attr(rustfmt, rustfmt_skip)]
         return Err(nom::Err::Error(error_position!(i, nom::ErrorKind::Custom(0))));
     }
 
-    slice = &slice[1..];
+    i = i.slice(1..);
     let mut string = StringFragments::new();
 
     loop {
-        match slice.chars().next() {
+        match i.fragment.chars().next() {
             Some('\"') => {
-                slice = &slice[1..];
+                i = i.slice(1..);
                 break;
             },
             Some('\\') => {
-                slice = &slice[1..];
-                match slice.chars().next() {
+                i = i.slice(1..);
+                match i.fragment.chars().next() {
                     Some('"') => {
                         string.push_char('"');
-                        slice = &slice[1..];
+                        i = i.slice(1..);
                     },
                     Some('\'') => {
                         string.push_char('\'');
-                        slice = &slice[1..];
+                        i = i.slice(1..);
                     },
                     Some('\\') => {
                         string.push_char('\\');
-                        slice = &slice[1..];
+                        i = i.slice(1..);
                     },
                     Some('r') => {
                         string.push_char('\r');
-                        slice = &slice[1..];
+                        i = i.slice(1..);
                     },
                     Some('n') => {
                         string.push_char('\n');
-                        slice = &slice[1..];
+                        i = i.slice(1..);
                     },
                     Some('t') => {
                         string.push_char('\t');
-                        slice = &slice[1..];
+                        i = i.slice(1..);
                     },
                     Some('u') => {
                         // We can't slice off the `u` yet because if we get an escape of
                         // `\u{invalid}` the invalid escape slice contains the `u`
-                        if slice.starts_with("u{") {
-                            let idx = slice.find('}').unwrap_or_else(|| slice.len());
+                        if i.fragment.starts_with("u{") {
+                            let idx = i.fragment.find('}').unwrap_or_else(|| i.input_len());
                             // Allow EOF to close to avoid eating string as other tokens
-                            let codepoint = u32::from_str_radix(&slice[2..idx], 16);
+                            let codepoint = u32::from_str_radix(&i.fragment[2..idx], 16);
                             if let Ok(Some(ch)) = codepoint.map(char::from_u32) {
                                 string.push_char(ch);
                             } else {
-                                string.push_invalid_escape(&slice[..idx + 1])
+                                string.push_invalid_escape(&i.fragment[..idx + 1])
                             }
-                            slice = &slice[idx + 2..];
+                            i = i.slice(idx + 2..);
                         } else {
                             string.push_invalid_escape('u'.to_string());
-                            slice = &slice[1..];
+                            i = i.slice(1..);
                         }
                     },
                     Some('{') => {
-                        slice = &slice[1..];
+                        i = i.slice(1..);
                         let mut tokens: Vec<Token> = vec![];
                         let mut depth = 1u32;
-                        while let Ok((tail, o)) = token(Span {
-                            // TODO: maintain span to maintain offset/line info
-                            offset: 0,
-                            line: 0,
-                            fragment: slice,
-                        }) {
-                            slice = tail.fragment;
+                        while let Ok((tail, o)) = token(i) {
+                            i = tail;
                             match o {
                                 Token::Symbol(_, Symbol::LeftCurlyBracket) => depth += 1,
                                 Token::Symbol(_, Symbol::RightCurlyBracket) => {
@@ -105,24 +100,21 @@ pub fn string_literal(i: Span) -> IResult<Span, Token> {
                     },
                     Some(c) => {
                         string.push_invalid_escape(c.to_string());
-                        slice = &slice[c.len_utf8()..];
+                        i = i.slice(c.len_utf8()..);
                     },
                     None => break, // Allow EOF to close to avoid eating string as other tokens
                 }
             },
             Some(_) => {
-                let idx = slice
+                let idx = i.fragment
                     .find(|ch| ch == '"' || ch == '\\')
-                    .unwrap_or_else(|| slice.len()); // Allow EOF to close
-                string.push_string(&slice[..idx]);
-                slice = &slice[idx..];
+                    .unwrap_or_else(|| i.fragment.len()); // Allow EOF to close
+                string.push_string(&i.fragment[..idx]);
+                i = i.slice(idx..);
             },
             None => break, // Allow EOF to close to avoid eating string as other tokens
         }
     }
 
-    Ok((
-        i.slice(i.input_len() - slice.len()..),
-        Token::Literal(i.offset, string.into()),
-    ))
+    Ok((i, Token::Literal(pos, string.into())))
 }
