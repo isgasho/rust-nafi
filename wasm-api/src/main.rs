69172e99eb5b1a8c5d54f234cd3efba70dcb8be7
Christopher Durham <cad97@cad97.com>
2018-04-29 20:44:03 -0400

    Remove memchr patch
    I just had an old version of libc before being stubbed for wasm.
    See https://github.com/BurntSushi/rust-memchr/pull/33

--- wasm-api/src/main.rs
+++ wasm-api/src/main.rs
@@ -4,20 +4,37 @@
 extern crate stdweb;
 extern crate nafi_lexer_repl;
 
-use stdweb::js_export;
-use std::fmt::Write;
+#[cfg(target_arch = "wasm32")]
+mod hide {
+    use super::*;
+    use stdweb::js_export;
+    use std::fmt::Write;
 
-#[js_export]
-fn lex(input: &str) -> String {
-    let tokens = nafi_lexer_repl::lex(input);
-    let mut out = String::new();
-    for tok in tokens {
-        writeln!(out, "{}", tok).unwrap();
+    #[js_export]
+    pub fn lex(input: &str) -> String {
+        let tokens = nafi_lexer_repl::lex(input);
+        let mut out = String::new();
+        for tok in tokens {
+            writeln!(out, "{}", tok).unwrap();
+        }
+        out
     }
-    out
+
+    pub fn main() {
+        stdweb::initialize();
+        stdweb::event_loop();
+    }
 }
 
+#[cfg(target_arch = "wasm32")]
+pub use hide::*;
+
+#[cfg(target_arch = "wasm32")]
 fn main() {
-    stdweb::initialize();
-    stdweb::event_loop();
+    hide::main()
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+fn main() {
+    println!("nafi-wasm-api is only useful on wasm32")
 }
