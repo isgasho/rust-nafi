64a804b8b314f79cb0401d97df9cbc3491013b6c
Christopher Durham <cad97@cad97.com>
2018-07-16 20:23:31 -0400

    Hide wasm-api from non-wasm

--- wasm-api/src/main.rs
+++ wasm-api/src/main.rs
@@ -2,26 +2,36 @@
 
 extern crate nafi_parser;
 extern crate ron;
+#[cfg(target_arch = "wasm32")]
 #[cfg_attr(target_arch = "wasm32", macro_use)]
 extern crate stdweb;
 
-use ron::ser::to_string_pretty;
 #[cfg(target_arch = "wasm32")]
-use stdweb::js_export;
+pub use hidden::*;
+#[cfg(target_arch = "wasm32")]
+mod hidden {
+    use ron::ser::to_string_pretty;
+    use stdweb::js_export;
 
-#[cfg_attr(target_arch = "wasm32", js_export)]
-#[cfg_attr(not(target_arch = "wasm32"), allow(unused))]
-fn parse(s: &str) -> String {
-    match nafi_parser::parse(s) {
-        Ok(parse) => match to_string_pretty(&parse, Default::default()) {
-            Ok(serialized) => serialized,
+    #[js_export]
+    fn parse(s: &str) -> String {
+        match nafi_parser::parse(s) {
+            Ok(parse) => match to_string_pretty(&parse, Default::default()) {
+                Ok(serialized) => serialized,
+                Err(err) => format!("{}", err),
+            },
             Err(err) => format!("{}", err),
-        },
-        Err(err) => format!("{}", err),
+        }
     }
 }
 
+#[cfg(target_arch = "wasm32")]
 fn main() {
     stdweb::initialize();
     stdweb::event_loop();
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+fn main() {
+    println!(">.>");
 }
