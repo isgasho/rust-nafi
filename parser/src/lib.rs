a1e12ea900334ee56970acc6be75559145af4d92
cad97 <cad97@cad97.com>
2018-10-14 03:21:28 -0400

    fmt

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -1,9 +1,9 @@
 pub use nafi_ast as ast;
 
 pub fn parse(s: &str) -> Result<ast::functions::FunctionExpression, Box<dyn std::error::Error>> {
-    use pest_deconstruct::FromPest;
     use crate::ast::parser::{Parser, Rule};
     use pest::Parser as Parse;
+    use pest_deconstruct::FromPest;
 
     let mut parse = Parser::parse(Rule::FunctionExpression, s)?;
     assert!(s[parse.as_str().len()..].chars().all(|c| c.is_whitespace()));
