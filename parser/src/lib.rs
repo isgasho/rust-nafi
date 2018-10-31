06d31272862243cabb290c0c883be54fb54904c1
cad97 <cad97@cad97.com>
2018-10-31 18:54:44 -0400

    Update pest-ast

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -2,10 +2,10 @@ pub use nafi_ast as ast;
 
 pub fn parse(s: &str) -> Result<ast::functions::FunctionExpression, Box<dyn std::error::Error>> {
     use crate::ast::parser::{Parser, Rule};
+    use from_pest::FromPest;
     use pest::Parser as Parse;
-    use pest_deconstruct::FromPest;
 
     let mut parse = Parser::parse(Rule::FunctionExpression, s)?;
     assert!(s[parse.as_str().len()..].chars().all(|c| c.is_whitespace()));
-    Ok(FromPest::from_pest(parse.next().unwrap()))
+    Ok(FromPest::from_pest(&mut parse).expect("infallible"))
 }
